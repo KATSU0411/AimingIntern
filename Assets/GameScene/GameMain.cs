0c3eee245ed977f405630d01a5fe058599adb335
KATSU0411 <taka.katsu411@gmail.com>
2019-09-10 18:03:52 +0900

    toriaezu

---
+++ Assets/GameScene/GameMain.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using HTTP;
+using Protocol;
+
+public class GameMain : MonoBehaviour
+{
+    private float m_time_elapsed = 0;
+    private bool m_flgMatched = false;
+
+    private GameObject GoodPiece;
+    private GameObject EvilPiece;
+    private GameObject UnknownPiece;
+    private GameObject field;
+    private RectTransform field_rect;
+
+    private float PIECE_SIZE;
+
+    private List<PieceInfo> Pieces;
+
+    // Use this for initialization
+    void Start()
+    {
+        //GetRoomInfo();
+        GoodPiece = (GameObject)Resources.Load("GoodPrefab");
+        EvilPiece = (GameObject)Resources.Load("EvilPrefab");
+        UnknownPiece = (GameObject)Resources.Load("UnknownPrefab");
+        field = GameObject.Find("Canvas/Panel/Image_field/Panel_piece");
+        field_rect = field.GetComponent<RectTransform>();
+        // 1コマ当たりのサイズ
+        PIECE_SIZE = field_rect.rect.width / 6.0f;
+
+        Pieces = new List<PieceInfo>();
+
+        InitPieces();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //m_time_elapsed += Time.deltaTime;
+        //if(m_time_elapsed >= 2)
+        //{
+        //    GetGameInfo();
+        //    GetRoomInfo();
+        //}
+    }
+
+    // 初期コマ8つの設置
+    void InitPieces()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            InitPiece(true, i + 2, 5);
+            InitPiece(false, i + 2, 6);
+        }
+    }
+
+    // フィールドの座標をUIの座標に変換
+    private Vector2 FieldPos(int x, int y)
+    {
+        return new Vector2(PIECE_SIZE * (x - 1) - (PIECE_SIZE * 3), -PIECE_SIZE * (y) + (PIECE_SIZE * 3));
+    }
+
+    // コマの設置
+    private GameObject InitPiece(bool flgGood, int x, int y)
+    {
+        GameObject piece;
+
+        if (flgGood) piece = Instantiate(GoodPiece);
+        else piece = Instantiate(EvilPiece);
+
+        // 子要素として追加
+        piece.transform.SetParent(field_rect, false);
+
+        // 座標の指定
+        RectTransform piece_rect = piece.GetComponent<RectTransform>();
+        piece_rect.sizeDelta = new Vector2(PIECE_SIZE, PIECE_SIZE);
+        piece_rect.localPosition = FieldPos(x, y);
+
+        var p = new PieceInfo();
+        p.point_x = x;
+        p.point_y = y;
+        p.kind = (flgGood ? "good" : "evil");
+
+        Pieces.Add(p);
+
+        return piece;
+    }
+
+    // Game情報取得
+    void GetGameInfo()
+    {
+
+        ApiClient.Instance.ResponseShowGame = ResponseShowGame;
+        var param = new RequestShowGame();
+        param.game_id = UserInfo.game_id;
+        ApiClient.Instance.RequestShowGame(param);
+    }
+    public void ResponseShowGame(ResponseShowGame response)
+    {
+    }
+
+
+    // Room情報取得
+    void GetRoomInfo()
+    {
+        ApiClient.Instance.ResponseShowRoom = ResponseShowRoom;
+        var param = new RequestShowRoom();
+        param.room_id = UserInfo.room_id;
+        ApiClient.Instance.RequestShowRoom(param);
+    }
+    public void ResponseShowRoom(ResponseShowRoom response)
+    {
+        // 情報保持
+        UserInfo.game_id = response.game_id;
+
+        if (response.status == "playing" && !m_flgMatched)
+        {
+            InitPieces();
+        }
+    }
+}
---
+++ Assets/GameScene/Piece.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using HTTP;
+using Protocol;
+
+public class Piece : MonoBehaviour {
+
+    private GameObject draggingObject;
+    private Transform canvasTran;
+
+	// Use this for initialization
+	void Start () {
+        canvasTran = transform.parent.parent.parent.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    // コマのドラッグイベント
+     public void OnBeginDrag(BaseEventData ev)
+    {
+        PointerEventData e = (PointerEventData)ev;
+        CreateDragObject();
+        draggingObject.transform.position = e.position;
+    }
+    public void OnDrag(BaseEventData ev)
+    {
+        PointerEventData e = (PointerEventData)ev;
+        this.gameObject.GetComponent<RectTransform>().position += new Vector3(e.delta.x, e.delta.y, 0.0f);
+    }
+     public void OnEndDrag(BaseEventData ev)
+    {
+        PointerEventData e = (PointerEventData)ev;
+        gameObject.GetComponent<Image>().color = Vector4.one;
+        Destroy(draggingObject);
+    }
+
+
+
+
+    public void OnPointerEnter(BaseEventData ev)
+    {
+        PointerEventData e = (PointerEventData)ev;
+        if (e.pointerDrag == null) return;
+        if (e.pointerDrag == this.gameObject) return;
+        this.gameObject.transform.Find("Image").GetComponent<Image>()
+                                        .color = new Vector4(1, 1, 0, 0.2f);
+    }
+    public void OnPointerExit(BaseEventData ev)
+    {
+        PointerEventData e = (PointerEventData)ev;
+        this.gameObject.transform.Find("Image").GetComponent<Image>()
+                                        .color = Vector4.zero;
+    }
+
+     // ドラッグオブジェクト作成
+    private void CreateDragObject()
+    {
+        draggingObject = new GameObject("Dragging Object");
+        draggingObject.transform.SetParent(canvasTran);
+        draggingObject.transform.SetAsLastSibling();
+        draggingObject.transform.localScale = Vector3.one;
+
+        // レイキャストがブロックされないように
+        CanvasGroup canvasGroup = draggingObject.AddComponent<CanvasGroup>();
+        canvasGroup.blocksRaycasts = false;
+
+        Image draggingImage = draggingObject.AddComponent<Image>();
+        Image sourceImage = GetComponent<Image>();
+
+        draggingImage.sprite = sourceImage.sprite;
+        draggingImage.rectTransform.sizeDelta = sourceImage.rectTransform.sizeDelta;
+        draggingImage.color = sourceImage.color;
+        draggingImage.material = sourceImage.material;
+
+        gameObject.GetComponent<Image>().color = Vector4.one * 0.6f;
+    }
+
+}
